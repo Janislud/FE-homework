653ec6aec004983458f7ef0d0d743a3e0ca1812d
Janislud <90716004+Janislud@users.noreply.github.com>
2023-05-10 12:19:33 +0200

    Homework5
    ;]

---
+++ Homework5/scripts/script.js
@@ -0,0 +1,49 @@
+
+
+
+
+//Написать программу, которая через prompt считывает число и выводит в консоль число равное 10% от введенного числа
+// {
+// const input = prompt(`Enter number`)
+// const number = Number(input)
+// if(Number.isNaN(number)) {
+//     console.log(`Not a number!`)
+// } else {
+//     console.log(`10% from you number is ${Math.round(number * 0.1)}`)
+// }
+// }
+
+// Написать программу, которая получает два числа и выводит наименьшее
+
+// const number1 = Number(prompt(`Enter first number`))
+// const number2 = Number(prompt(`Enter second number`))
+
+// if (number1 < number2) {
+//     console.log(number1)
+//     }
+//     else {
+//         console.log(number2)
+//     }
+
+
+// Написать программу, которая считывает через prompt число и выводит одно из следующих сообщений: ‘Число меньше 100’, ‘Число больше 100’ или ‘Число равно 100’
+// const num =+prompt("Ведите чесло")
+// if (num > 100) {
+//     console.log(`Число больше 100`)
+// } else if (num < 100) {
+//     console.log(`Число меньше 100`)
+// } else {
+//     console.log(`Число равно 100`)
+// }
+
+//Написать программу, которая запрашивает у пользователя его имя и возраст (в годах) и выводит в консоль сообщение ‘Hello, <name>’, если пользователь совершеннолетний, или ‘Hi, <name>’, если пользователь несовершеннолетний.
+
+// const name =prompt("Enter name")
+// const age =+prompt("Enter age")
+
+// if(age >= 18){
+// console.log("Hello", name )
+// }else {
+// console.log("Hi",name)
+// }
+
